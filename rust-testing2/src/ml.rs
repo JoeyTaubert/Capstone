3abb2c2b4ec5ab1c33a3c284dc1817c93dae49d7
JoeyTaubert <joey.taubert@comcast.net>
2024-02-11 15:07:39 -0500

    fix RNN

--- rust-testing2/src/ml.rs
+++ rust-testing2/src/ml.rs
@@ -1,96 +1,202 @@
 // ML - RNN
 /// Reference: codemoon on YouTube, https://github.com/codemoonsxyz/neural-net-rs
 use rand::Rng;
+use std::f64::consts::E;
 
 
-
+#[derive(Debug,Clone)]
 pub struct Matrix {
     pub rows: usize,
     pub cols: usize,
     pub data: Vec<f64>
 }
 
-pub fn randomm(rows: usize, cols: usize) -> Matrix {
-    let mut rbuffer = Vec::<f64>::with_capacity(rows * cols);
+impl Matrix{
+    pub fn randomm(rows: usize, cols: usize) -> Matrix {
+        let mut rbuffer = Vec::<f64>::with_capacity(rows * cols);
 
-    // Generate a random number for each value that will be in the Matrix 
-    for _ in 0..rows*cols {
-        let num = rand::thread_rng().gen_range(0.0..1.0);
+        // Generate a random number for each value that will be in the Matrix 
+        for _ in 0..rows*cols {
+            let num = rand::thread_rng().gen_range(0.0..1.0);
 
-        rbuffer.push(num);
+            rbuffer.push(num);
+        }
+
+        // Build the matrix and return it
+        Matrix{rows,cols,data:rbuffer}
     }
 
-    // Build the matrix and return it
-    Matrix{rows,cols,data:rbuffer}
-}
+    pub fn addm (&self, other: &Matrix) -> Matrix {
+        if self.rows != other.rows || self.cols != other.cols {
+            panic!("Attempted to add matrix of incorrect dimensions") //// REPLACE THIS FOR PRODUCTION
+        }
+        
+        let mut abuffer = Vec::<f64>::with_capacity(self.rows * self.cols);
 
-pub fn addm (&self, other: &Matrix) -> Matrix {
-    if self.rows != other.rows || self.cols != other.cols {
-        panic!("Attempted to add matrix of incorrect dimensions") //// REPLACE THIS FOR PRODUCTION
-    }
-    
-    let mut abuffer = Vec::<f64>::with_capacity(self.rows * self.cols);
+        for i in 0..self.data.len() {
+            let result = self.data[i] + other.data[i];
 
-    for i in 0..self.data.len() {
-        let result = self.data[i] + other.data[i];
+            abuffer.push(result)
+        }
 
-        abuffer.push(result)
-    }
-
-    Matrix {
-        rows: self.rows,
-        cols: self.cols,
-        data: abuffer
-    }
-}
-
-pub fn subtractm(&self, other: &Matrix) -> Matrix {
-
-    // Verify that the matrices are of equal dimensions
-    assert!(
-        self.rows == other.rows && self.cols == other.cols,
-        "Cannot subtract matricies with different dimensions"
-    );
-
-    let mut sbuffer = Vec::<f64>::with_capacity(self.rows * self.cols);
-
-    for i in 0..self.data.len() {
-
-        let result = self.data[i] - other.data[i];
-
-        sbuffer.push(result);
-    }
-
-    Matrix {
-        rows: self.rows,
-        cols: self.cols,
-        data: sbuffer
-    }
-}
-
-pub fn dot_multiply(&self, other: &Matrix) -> Matrix {
-    if self.cols != other.rows {
-        panic!("Attempted to multiply by matrix of incorrect dimensions!") //// REPLACE THIS FOR PRODUCTION
-    }
-
-    let mut result_data = vec![0.0; self.rows * other.cols];
-
-    for i in 0..self.rows {
-        for j in 0..other.cols {
-            let mut sum = 0.0;
-            for k in 0..self.cols {
-                sum += self.data[i * self.cols + k] * other.data[k * other.cols + j];
-            }
-            result_data[i * other.cols + j] = sum;
+        Matrix {
+            rows: self.rows,
+            cols: self.cols,
+            data: abuffer
         }
     }
 
-    Matrix {
-        rows: self.rows,
-        cols: other.cols,
-        data: result_data,
+    pub fn subtractm(&self, other: &Matrix) -> Matrix {
+
+        // Verify that the matrices are of equal dimensions
+        assert!(
+            self.rows == other.rows && self.cols == other.cols,
+            "Cannot subtract matricies with different dimensions"
+        );
+
+        let mut sbuffer = Vec::<f64>::with_capacity(self.rows * self.cols);
+
+        for i in 0..self.data.len() {
+
+            let result = self.data[i] - other.data[i];
+
+            sbuffer.push(result);
+        }
+
+        Matrix {
+            rows: self.rows,
+            cols: self.cols,
+            data: sbuffer
+        }
+    }
+
+    pub fn dot_multiply(&self, other: &Matrix) -> Matrix {
+        if self.cols != other.rows {
+            panic!("Attempted to multiply by matrix of incorrect dimensions!") //// REPLACE THIS FOR PRODUCTION
+        }
+
+        let mut result_data = vec![0.0; self.rows * other.cols];
+
+        for i in 0..self.rows {
+            for j in 0..other.cols {
+                let mut sum = 0.0;
+                for k in 0..self.cols {
+                    sum += self.data[i * self.cols + k] * other.data[k * other.cols + j];
+                }
+                result_data[i * other.cols + j] = sum;
+            }
+        }
+
+        Matrix {
+            rows: self.rows,
+            cols: other.cols,
+            data: result_data,
+        }
+    }
+
+    pub fn elementwise_multiply(&self, other: &Matrix) -> Matrix {
+        if self.rows != other.rows || self.cols != other.cols {
+            panic!("Attempted to multiply by Matrix of incorrect dimensions") //// REPLACE FOR PROD
+        }
+
+        let mut result_data = vec![0.0; self.cols * self.rows];
+        for i in 0..self.data.len() { //codemoon left a note to double check this, I believe self.data.len() may need to be reduced by 1, if anything
+            result_data[i] = self.data[i] *  other.data[i]
+        }
+
+        Matrix {
+            rows: self.rows,
+            cols: self.cols,
+            data: result_data,
+        }
+    }
+
+    pub fn new(rows: usize, cols: usize, data: Vec<f64>) -> Matrix {
+        assert!(data.len()-1 != rows * cols, "Invalid Size");
+        Matrix { rows, cols, data }
+    }
+
+    pub fn zeros(rows: usize, cols: usize) -> Matrix {
+        Matrix { rows, cols, data: vec![0.0; cols * rows] }
+    }
+
+    pub fn transpose(&self) -> Matrix {
+        let mut tbuffer = vec![0.0; self.cols * self.rows];
+
+        for i in 0..self.rows {
+            for j in 0..self.cols {
+                tbuffer[j * self.rows + i] = self.data[i * self.cols + j];
+            }
+        }
+
+        Matrix {
+            rows: self.cols, 
+            cols: self.rows, 
+            data: tbuffer,
+        }
+    }
+
+    pub fn map(&mut self, func: fn(&f64) -> f64) -> Matrix {
+        let mut result = Matrix {
+            rows: self.rows,
+            cols: self.cols,
+            data: Vec::with_capacity(self.data.len()),
+        };
+
+        result.data.extend(self.data.iter().map(|&val| func(&val))); //iterates over each element of the matrix and applies the function
+
+        result
+    }
+} 
+
+/// Other Implementations for the Matrix type
+impl From<Vec<f64>> for Matrix {
+    fn from(vec: Vec<f64>) -> Self {
+        let rows = vec.len();
+        let cols = 1;
+        Matrix {
+            rows,
+            cols,
+            data: vec,
+        }
     }
 }
+
+impl PartialEq for Matrix {
+    fn eq(&self, other: &Self) -> bool {
+        self.rows == other.rows && self.cols == other.cols && self.data == other.data
+    }
+}
+
+/// Not sure if this is even needed as I am not performing the "tests"
+//impl fmt::Display for Matrix {
+//    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+//        for row in 0..self.rows {
+//            for col in 0..self.cols {
+//                write!(f, "{}", self.data[row * self.cols + col])?;
+//                if col < self.cols - 1 {
+//                    write!(f, "\t")?; // Separate columns with a tab
+//                }
+//            }
+//            writeln!(f)?; // Move to the next line after each row
+//        }
+//        Ok(())
+//}
+
+
+
+/// Struct for Activation functions, such as sigmoid, ReLU, GELU, etc.
+#[derive(Clone,Copy,Debug)]
+pub struct Activation {
+    pub function: fn(&f64) -> f64,
+    pub derivative: fn(&f64) -> f64,
+}
+
+/// Constant for sigmoid activation function
+pub const SIGMOID: Activation = Activation {
+    function: |x| 1.0 / (1.0 + E.powf(-x)), 
+    derivative: |x| x * (1.0 - x), // Derivative is used to access weights in the back propagation process
+};
 
 pub struct NNetwork {
     layers: Vec<usize>, // amount of neurons per layer ex: [2, 4, 1]
@@ -109,8 +215,8 @@ impl NNetwork {
 
         // Iterate over layers of the neural network
         for i in 00..layers.len() -1 {
-            weights.push(randomm(layers[i+1], layers[i]));
-            biases.push(randomm(layers[i+1], 1));
+            weights.push(Matrix::randomm(layers[i+1], layers[i])); 
+            biases.push(Matrix::randomm(layers[i+1], 1)); 
         }
 
         // Initialize the network
@@ -137,15 +243,17 @@ impl NNetwork {
             // Apply feed forward algorithm
             current = self.weights[i]
             .dot_multiply(&current)
-            .add(&self.biases[i]).map(self.activation.function);
+            .addm(&self.biases[i]).map(self.activation.function);
 
             // Return output of network
             self.data.push(current.clone());
         }
+        current
     }
 
     pub fn back_propagate(&mut self, inputs: Matrix, targetrs: Matrix) {
-        let mut errors = targets.subtract(&inputs);
+        let mut gradients = inputs.clone().map(self.activation.derivative);
+        let mut errors = targetrs.subtractm(&inputs);
 
         // Determine which parts of the network are responsible for errors by iterating over each layer
         // Need to multiply activation function by its derivative to access weights and biases
@@ -154,8 +262,8 @@ impl NNetwork {
         for i in (0..self.layers.len() - 1).rev() {
             gradients = gradients.elementwise_multiply(&errors).map(|x| x * 0.5);
 
-            self.weights[i] = self.weights[i].add(&gradients.dot_multiply(&self.data[i].transpose()));
-            self.biases[i] = self.biases[i].add(&gradients);
+            self.weights[i] = self.weights[i].addm(&gradients.dot_multiply(&self.data[i].transpose()));
+            self.biases[i] = self.biases[i].addm(&gradients);
 
             errors = self.weights[i].transpose().dot_multiply(&errors);
             gradients = self.data[i].map(self.activation.derivative);
